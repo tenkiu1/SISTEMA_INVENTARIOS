0894b3fc2cb17ead7357da54c37f8f023a6c7aa2
tenkiu1 <150759059+tenkiu1@users.noreply.github.com>
2023-11-22 16:25:32 -0500

    nuevo

---
+++ src/main/java/com/mycompany/sistema_inventarios/Sistema_Inventarios.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sistema_inventarios;
+
+/**
+ *
+ * @author jorge
+ */
+public class Sistema_Inventarios {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
