03030e3033ccc0ea66d06667bb1201b182ff8e76
tenkiu1 <150759059+tenkiu1@users.noreply.github.com>
2023-11-22 21:07:25 -0500

    nuevo

--- src/main/java/com/mycompany/sistema_inventarios/Sistema_Inventarios.java
+++ src/main/java/com/mycompany/sistema_inventarios/Sistema_Inventarios.java
@@ -4,14 +4,38 @@
 
 package com.mycompany.sistema_inventarios;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 /**
  *
  * @author jorge
  */
 public class Sistema_Inventarios {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-        System.out.println("Hello World!");
+  public static void main(String[] args) {
+        // Información de conexión
+        String url = "jdbc:mysql://localhost:3306/inventarios";
+        String usuario = "root";
+        String contraseña = "";
+
+        // Intentar establecer la conexión
+        try {
+            Connection conexion = DriverManager.getConnection(url, usuario, contraseña);
+
+            // La conexión ha sido establecida con éxito
+            System.out.println("Conexión exitosa a la base de datos");
+
+            // Realizar operaciones en la base de datos...
+
+            // Cerrar la conexión cuando hayas terminado
+            conexion.close();
+        } catch (SQLException e) {
+            // Manejar errores de conexión
+            e.printStackTrace();
+        }
     }
-}
+  }
